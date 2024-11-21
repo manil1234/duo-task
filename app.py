0bc547468852c2f411a1e95979d386f9f0393497
Manil <manilkohombage@gmail.com>
2024-11-21 11:44:47 +0000

    line error

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ def home():
     name = getenv('YOUR_NAME')
     if name is None:
       name = "friend"
-    return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in {hostname}.</h2>\n I ammended this part too cos I'm a G\n\n 
-    This is actually the development one, still working on it!"
+    return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in {hostname}.</h2>\n I ammended this part too cos I'm a G\n\n This is actually the development one, still working on it!"
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', port=5500, debug=True)
